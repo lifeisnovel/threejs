9382c94f31de5726aab6be4ed4ceaf3b93e1a232
yPeA <ariz333@naver.com>
2022-01-19 04:07:45 +0900

    modify outputDir dist to docs

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-	publicPath: process.env.NODE_ENV === 'production' ? '/threejs' : '/'
+	publicPath: process.env.NODE_ENV === 'production' ? '/threejs' : '/',
+	outputDir: 'docs'
 }
